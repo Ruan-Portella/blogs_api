e9aaa52394b7c023038c907b9bb946c9907259ff
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-07-12 10:34:26 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+// ...
+
+const app = express();
+
+// não remova ou mova esse endpoint
+app.get('/', (_request, response) => {
+  response.send();
+});
+
+app.use(express.json());
+
+// ...
+
+// É importante exportar a constante `app`,
+// para que possa ser utilizada pelo arquivo `src/server.js`
+module.exports = app;
