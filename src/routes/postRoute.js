df766967b49678505b6fd9ff886e2a1f634f5fda
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 20:41:07 -0300

    Feat: Cria rota put

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,10 +1,12 @@
 const postRoute = require('express').Router();
 const postController = require('../controllers/postController');
 const validatePost = require('../middlewares/validatePost');
+const validatePostUpdated = require('../middlewares/validatePostUpdated');
 const validateToken = require('../middlewares/validateToken');
 
 postRoute.post('/', validateToken, validatePost, postController.createPost);
 postRoute.get('/', validateToken, postController.getPosts);
 postRoute.get('/:id', validateToken, postController.getPostsById);
+postRoute.put('/:id', validateToken, validatePostUpdated, postController.updatePost);
 
 module.exports = postRoute;
