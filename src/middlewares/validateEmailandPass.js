3c5c522cdecd73d2300d327d1afc184c5cba1559
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 15:56:11 -0300

    Feat: Melhora validação email e password

--- src/middlewares/validateEmailandPass.js
+++ src/middlewares/validateEmailandPass.js
@@ -3,6 +3,14 @@ const validateEmailAndPass = (req, res, next) => {
     if (!email || !password) {
         return res.status(400).json({ message: 'Some required fields are missing' });
     }
+    if (!email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i)) {
+        return res.status(400).json({ message: '"email" must be a valid email' });
+    }
+    if (password.length < 6) {
+        return res.status(400).json(
+            { message: '"password" length must be at least 6 characters long' },
+);
+    }
     next();
 };
 
