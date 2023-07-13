c3b17e324090049b2e826dac448362bc1e97c15c
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 16:31:06 -0300

    Feat: Usa getUser no Route e Finaliza Req 05

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,7 +2,9 @@ const userRouter = require('express').Router();
 const userController = require('../controllers/userController');
 const validateDisplayName = require('../middlewares/validateDisplayName');
 const validateEmailAndPass = require('../middlewares/validateEmailandPass');
+const validateToken = require('../middlewares/validateToken');
 
 userRouter.post('/', validateEmailAndPass, validateDisplayName, userController.signUp);
+userRouter.get('/', validateToken, userController.getUsers);
 
 module.exports = userRouter;
