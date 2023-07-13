2fd12c9c375a0aa86b53d1b7ff623b5cbd07d7b4
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 19:59:52 -0300

    Feat: Coloca o id na assinatura do token

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,7 +4,12 @@ const { User } = require('../models');
 const signIn = async (email, password) => {
     const user = await User.findOne({ where: { email } });
     if (!user || user.password !== password) return { message: 'Invalid fields' };
-    const token = jwt.sign(email, process.env.JWT_SECRET, { algorithm: 'HS256' });
+    const token = jwt.sign(
+{ email, id: user.dataValues.id },
+         process.env.JWT_SECRET,
+
+{ algorithm: 'HS256' },
+);
     return token;
 };
 
