a8474079fd2caeaababff8ee42f355fe94b312b8
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 15:33:50 -0300

    Feat: Cria Services para login

---
+++ src/services/authService.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const signIn = async (email, password) => {
+    const user = await User.findOne({ where: { email } });
+    if (!user || user.password !== password) return { message: 'Invalid fields' };
+    const token = jwt.sign(email, process.env.JWT_SECRET, { algorithm: 'HS256' });
+    return token;
+};
+
+module.exports = {
+    signIn,
+};
