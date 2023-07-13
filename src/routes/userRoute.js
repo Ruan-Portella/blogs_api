90a127a00a2aec4600f1bdd016d2ab433e61a080
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 16:51:15 -0300

    Feat: Cria rota /user/:id

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,5 +6,6 @@ const validateToken = require('../middlewares/validateToken');
 
 userRouter.post('/', validateEmailAndPass, validateDisplayName, userController.signUp);
 userRouter.get('/', validateToken, userController.getUsers);
+userRouter.get('/:id', validateToken, userController.getUserById);
 
 module.exports = userRouter;
