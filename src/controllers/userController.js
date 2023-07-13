9c005005b5906f1880bd781fc8e789864003575b
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 16:51:03 -0300

    Feat: getUserById no controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,4 +12,11 @@ const getUsers = async (req, res) => {
     return res.status(200).json(result);
 };
 
-module.exports = { signUp, getUsers };
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    const result = await userService.getUserById(id);
+    if (result.message) return res.status(404).json({ message: result.message });
+    return res.status(200).json(result);
+};
+
+module.exports = { signUp, getUsers, getUserById };
