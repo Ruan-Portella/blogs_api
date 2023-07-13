d01b3417d162c4a02ef47b3c50466c07de3c7ca0
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 18:23:29 -0300

    Feat: Cria model de PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, _DataTypes) => {
+    const PostCategory = sequelize.define(
+'PostCategory', 
+    { postId: { type: sequelize.INTEGER, primaryKey: true },
+      categoryId: { type: sequelize.INTEGER, primaryKey: true } },
+{ timestamps: false,
+        tableName: 'posts_categories',
+        underscored: true },
+);
+
+PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, { as: 'categories',
+        through: PostCategory,
+        foreignKey: 'postId',
+        otherKey: 'categoryId' });
+    models.Category.belongsToMany(models.BlogPost, { as: 'blogPosts',
+        through: PostCategory,
+        foreignKey: 'categoryId',
+        otherKey: 'postId' });
+}; return PostCategory;
+};
