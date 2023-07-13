fdb03905351edfc7624daa013f293493ddb53b05
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 17:56:01 -0300

    Feat: Utiliza RouteCategory no app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const AuthRouter = require('./routes/authRoute');
 const userRouter = require('./routes/userRoute');
+const categoryRoute = require('./routes/categoryRoute');
 
 // ...
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use('/', AuthRouter);
 app.use('/user', userRouter);
+app.use('/categories', categoryRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
