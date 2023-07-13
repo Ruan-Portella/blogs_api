f9b15ad0c0001c203a934b27e64130a0c4f9584f
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 20:58:05 -0300

    Feat: Cria a rota delete

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -3,10 +3,12 @@ const postController = require('../controllers/postController');
 const validatePost = require('../middlewares/validatePost');
 const validatePostUpdated = require('../middlewares/validatePostUpdated');
 const validateToken = require('../middlewares/validateToken');
+const validateTokenCategory = require('../middlewares/validateTokenCategory');
 
 postRoute.post('/', validateToken, validatePost, postController.createPost);
 postRoute.get('/', validateToken, postController.getPosts);
 postRoute.get('/:id', validateToken, postController.getPostsById);
 postRoute.put('/:id', validateToken, validatePostUpdated, postController.updatePost);
+postRoute.delete('/:id', validateTokenCategory, postController.deletePost);
 
 module.exports = postRoute;
