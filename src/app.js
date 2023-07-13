d034d00ddcbe1a52a9f92e62ef3531580b6fe5da
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 19:59:22 -0300

    Feat: Utiliza o PostRoute no app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AuthRouter = require('./routes/authRoute');
 const userRouter = require('./routes/userRoute');
 const categoryRoute = require('./routes/categoryRoute');
+const postRoute = require('./routes/postRoute');
 
 // ...
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use('/', AuthRouter);
 app.use('/user', userRouter);
 app.use('/categories', categoryRoute);
+app.use('/post', postRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
