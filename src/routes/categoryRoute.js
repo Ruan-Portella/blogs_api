e5c40055e56e0603bd5ac97ddb2bc815ee55ec0d
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-13 18:06:35 -0300

    Feat: Cria a rota /get na category

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,6 +1,7 @@
 const categoryRoute = require('express').Router();
 const categoryController = require('../controllers/categoryController');
 const validateCategoryName = require('../middlewares/validateCategoryName');
+const validateToken = require('../middlewares/validateToken');
 const validateTokenCategory = require('../middlewares/validateTokenCategory');
 
 categoryRoute.post(
@@ -9,5 +10,6 @@ validateCategoryName,
 validateTokenCategory, 
 categoryController.createCategory,
 );
+categoryRoute.get('/', validateToken, categoryController.getAllCategories);
 
 module.exports = categoryRoute;
